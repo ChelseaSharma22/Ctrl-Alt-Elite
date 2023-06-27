b7c90f9427e4b1746b13d28ac1e35993c34a7ae6
aflak chemali <aflak.chemali@yahoo.com>
2023-06-27 08:53:55 -0700

    2

--- post.js
+++ post.js
@@ -41,9 +41,81 @@ const filterByMostLikes = (posts) => {
   return posts.sort((a, b) => b.likes.length - a.likes.length);
 };
 
+
+//my code
+const loginData = loggedIn();
+const currentUser = loginData.username;
+const userFullName = document.getElementById("fullName");
+const userCreationDate = document.getElementById("createdAt");
+const signoutBtn = document.getElementById("signoutBtn");
+const bio = document.getElementById("bio");
+const fullNameInput = document.getElementById("fullNameInput");
+const bioInput = document.getElementById("bioInput");
+const saveEditsBtn = document.getElementById("editSaveBtn");
+
+
+window.addEventListener("load", function () {
+    moreInfo();
+   
+    saveEditsBtn.onclick = updateProfile;
+    document.getElementById("name").innerHTML = "@" + currentUser;
+  
+
+
+});
+
+// Update Profile when Edit Profile Saved
+function updateProfile(event) {
+    event.preventDefault();
+
+    const options = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${loginData.token}`,
+        },
+        body: JSON.stringify({
+            bio: bioInput.value,
+            fullName: fullNameInput.value,
+        }),
+    };
+    fetch(
+        "https://microbloglite.herokuapp.com/" + "api/users/" + loginData.username,
+        options
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            setTimeout(function () {
+                location.reload();
+            }, 1000); // Refreshes the page.
+        });
+}
+
+// Grab Full Name
+function moreInfo() {
+    fetch(`https://microbloglite.herokuapp.com/api/users/${loginData.username}`, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${loginData.token}`,
+            "Content-type": "application/json; charset=UTF-8",
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            userFullName.innerHTML = data.fullName;
+            bio.innerHTML = data.bio;
+            
+        });
+}
+
+
+//my code 
 // Function to fetch posts
 const fetchPosts = () => {
   if (loggedIn() !== null) {
+    console.log("loged in !")
     const options = {
       method: "GET",
       headers: {
@@ -185,90 +257,93 @@ const createPost = (content) => {
   
 };
 
-// Check if the user is logged in
+//Check if the user is logged in
 const userLoggedIn = loggedIn();
 
-if (userLoggedIn) {
-  // Event listeners
-  recentBtn.addEventListener("click", () => {
-    fetchRecentPosts().then((data) => {
-      console.log(data);
-      updateUI(data);
-    });
-  });
+ if (userLoggedIn) {
+//   // Event listeners
+//  recentBtn.addEventListener("click", () => {
+//   fetchRecentPosts().then((data) => {
+//        console.log(data);
+//       updateUI(data);
+//     });
+//  });
 
-  logoutBtn.addEventListener("click", logout);
-  logoutBtn.addEventListener(alert, "hi")
+   logoutBtn.addEventListener("click", logout);
 
-  // Retrieve recent posts and update UI
-  fetchRecentPosts().then((data) => {
-    console.log(data);
-    updateUI(data);
-  });
+
+   
+ 
+
+//   // Retrieve recent posts and update UI
+//   fetchRecentPosts().then((data) => {
+//     console.log(data);
+//     updateUI(data);
+//   });
   
 
-  // Add event listener for form submission
-  postForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const postContent = createPostInput.value;
-    createPost(postContent);
-  });
+//   // Add event listener for form submission
+//   postForm.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     const postContent = createPostInput.value;
+//     createPost(postContent);
+//   });
 
-  // Function to update the UI with posts
-  const updateUI = (allPosts) => {
+//   // Function to update the UI with posts
+//   const updateUI = (allPosts) => {
 
-    let posts=allPosts.filter(post=>{
-      if((new RegExp(/.*donut/)).test(post.username)){
-        post.username=post.username.substr(0,post.username.length-5);
-        //console.log(post.username)
-        return post
-      }
-    })
+//     let posts=allPosts.filter(post=>{
+//       if((new RegExp(/.*donut/)).test(post.username)){
+//         post.username=post.username.substr(0,post.username.length-5);
+//         //console.log(post.username)
+//         return post
+//       }
+//     })
 
-    // if(isShowAll){
-    //   posts=allPosts
-    // }
-    postsContainer.innerHTML = "";
+//     // if(isShowAll){
+//     //   posts=allPosts
+//     // }
+//     postsContainer.innerHTML = "";
 
-    posts.forEach((post) => {
-      const { text, likes, username, createdAt, _id } = post;
+//     posts.forEach((post) => {
+//       const { text, likes, username, createdAt, _id } = post;
 
-      const postDiv = document.createElement("div");
-      postDiv.className = "post";
+//       const postDiv = document.createElement("div");
+//       postDiv.className = "post";
 
-      const postContent = document.createElement("div");
-      postContent.className = "post-content";
-      postContent.innerHTML = `<p>${text.replaceAll("<","&lt;").replaceAll(">","&rt;")}</p>`;
+//       const postContent = document.createElement("div");
+//       postContent.className = "post-content";
+//       postContent.innerHTML = `<p>${text.replaceAll("<","&lt;").replaceAll(">","&rt;")}</p>`;
 
-      const postDetails = document.createElement("div");
-      postDetails.className = "post-details";
-      postDetails.innerHTML = `<span class="post-likes">${likes.length} likes</span> | <span class="post-author">${username}</span> | <span class="post-date">${createdAt}</span>`;
+//       const postDetails = document.createElement("div");
+//       postDetails.className = "post-details";
+//       postDetails.innerHTML = `<span class="post-likes">${likes.length} likes</span> | <span class="post-author">${username}</span> | <span class="post-date">${createdAt}</span>`;
 
-      const likeBtn = document.createElement("button");
-      likeBtn.id = "like*"+_id;
-      likeBtn.addEventListener("click", () => likePost(post));
-      likeBtn.textContent = "Like";
+//       const likeBtn = document.createElement("button");
+//       likeBtn.id = "like*"+_id;
+//       likeBtn.addEventListener("click", () => likePost(post));
+//       likeBtn.textContent = "Like";
 
-      const dislikeBtn = document.createElement("button");
-      dislikeBtn.id ="dislike*"+ _id;
-      dislikeBtn.addEventListener("click", () => dislikePost(post));
-      dislikeBtn.textContent = "Dislike";
+//       const dislikeBtn = document.createElement("button");
+//       dislikeBtn.id ="dislike*"+ _id;
+//       dislikeBtn.addEventListener("click", () => dislikePost(post));
+//       dislikeBtn.textContent = "Dislike";
 
-      const deleteBtn = document.createElement("button");
-      deleteBtn.id = "delete*"+ _id;
-      deleteBtn.addEventListener("click", () => deleteUserPost(post));
-      deleteBtn.textContent = "Delete";
+//       const deleteBtn = document.createElement("button");
+//       deleteBtn.id = "delete*"+ _id;
+//       deleteBtn.addEventListener("click", () => deleteUserPost(post));
+//       deleteBtn.textContent = "Delete";
 
-      postDiv.appendChild(postContent);
-      postDiv.appendChild(postDetails);
-      postDiv.appendChild(likeBtn);
-      postDiv.appendChild(dislikeBtn);
-      postDiv.appendChild(deleteBtn);
+//       postDiv.appendChild(postContent);
+//       postDiv.appendChild(postDetails);
+//       postDiv.appendChild(likeBtn);
+//       postDiv.appendChild(dislikeBtn);
+//       postDiv.appendChild(deleteBtn);
 
-      postsContainer.appendChild(postDiv);
+//       postsContainer.appendChild(postDiv);
 
-    });
-  };
+//     });
+//   };
 
 
 } else {
