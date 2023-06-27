21c3b13f14e17d1d11c411644f06e8f0d07eb352
aflak chemali <aflak.chemali@yahoo.com>
2023-06-27 15:51:07 -0700

    update

--- post.js
+++ post.js
@@ -12,14 +12,14 @@ const imghidden = document.getElementById("imghidden");
 const input = document.getElementById("addimg");
 
 
-/* 
-// emoji group
+
+/* // emoji group
 let emojiGroup=[]
-let emojisTag = document.getElementById("emojis").getElementsByTagName("img");
+// let emojisTag = document.getElementById("emojis").getElementsByTagName("img");
 for(dom of emojisTag ){ //replace dom loop
   let _this=dom;
   dom.addEventListener("click",function(){
-    console.log(_this.id)
+    //console.log(_this.id)
     document.getElementById("createPost").value+=("%"+_this.id+"%")
   })
   let tempemj={}
@@ -29,14 +29,152 @@ for(dom of emojisTag ){ //replace dom loop
   emojiGroup.push(tempemj)
 } */
 
+// Check if the user is logged in
+const userLoggedIn = loggedIn();
+// the page work normal if the user login
+if (userLoggedIn) {
+  // Event listeners
+  recentBtn.addEventListener("click", () => {//recent button to refresh the post
+    fetchRecentPosts().then((data) => {
+     // console.log(data);
+      updateUI(data);
+    });
+  });
+
+  logoutBtn.addEventListener("click", logout);
+
+  // Retrieve recent posts and update UI
+  fetchRecentPosts().then((data) => {
+    //console.log(data);
+    updateUI(data);
+  });
+  
+
+  // Add event listener for form submission to create post
+  postForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const postContent = createPostInput.value;
+    createPost(postContent);
+  });
+
+  // Function to update the UI with posts
+  function updateUI(allPosts) {
+
+    //filter the post only for our website
+    let posts=allPosts.filter(post=>{
+      if((new RegExp(/.*donut/)).test(post.username)){
+        post.username=post.username.substr(0,post.username.length-5);
+        //console.log(post.username)
+        return post
+      }
+    })
+
+    //using forEach to loop and display the posts
+    postsContainer.innerHTML = "";
+    posts.forEach((post) => {
+      const { text, likes, username, createdAt, _id } = post;
+  
+      let contentHtml = ""
+
+      if(text.split("data:image").length==1){ //means no image been choose
+        //console.log(text)
+        contentHtml=`<p>${text.replaceAll("<","&lt;").replaceAll(">","&rt;").replace("data:image","")}</p>` //avoid comment have html style to effect our own html
+      }else{//there is image been choose
+       // console.log(text)
+        let textMain=text.split("data:image")[0]; //using "data:image" to separate so the base64 can read the url of the image
+        let textImg=text.split("data:image")[1];
+        
+        //if emoji use 
+          contentHtml=`<p>${textMain.replaceAll("<","&lt;").replaceAll(">","&rt;")}</p>`
+            +(textImg!=undefined?`<br><img src="data:image${textImg}">`:"")
+      }
+
+      /*   for(e of emojiGroup){//using for of to change the emoji text(id) back to emoji image
+          let emj=e
+        let eName=emj.emojiName
+        contentHtml=contentHtml.replaceAll("%"+eName+"%",`<img width=25 height=25 src="${emj.src}" class="emoji ${emj.id}"/>`)
+      } */
+      
+      const postDiv = document.createElement("div");
+      postDiv.className = "post";
+
+      const postContent = document.createElement("div");
+      postContent.className = "post-content";
+      postContent.innerHTML = contentHtml;
+
+      const postDetails = document.createElement("div");
+      postDetails.className = "post-details";
+      postDetails.innerHTML = `<span class="post-likes">${likes.length} likes</span> | <span class="post-author">${username}</span> | <span class="post-date">${createdAt}</span>`;
+
+      const likeBtn = document.createElement("button");
+      likeBtn.id = "like*"+_id;
+      likeBtn.addEventListener("click", () => likePost(post));
+      likeBtn.textContent = "Like";
+
+      const dislikeBtn = document.createElement("button");
+      dislikeBtn.id ="dislike*"+ _id;
+      dislikeBtn.addEventListener("click", () => dislikePost(post));
+      dislikeBtn.textContent = "Dislike";
+
+      const deleteBtn = document.createElement("button");
+      deleteBtn.id = "delete*"+ _id;
+      deleteBtn.addEventListener("click", () => deleteUserPost(post));
+      deleteBtn.textContent = "Delete";
+
+      postDiv.appendChild(postContent);
+      postDiv.appendChild(postDetails);
+      postDiv.appendChild(likeBtn);
+      postDiv.appendChild(dislikeBtn);
+      postDiv.appendChild(deleteBtn);
+
+      postsContainer.appendChild(postDiv);
+
+    });
+  };
+
+
+} else {
+  window.location.assign("./login.html");//send back to the login page
+}
+  
+
+//for adding image to the post
+if (typeof FileReader === "undefined") { //check able to use the fileReader
+  alert("This browser not support FileReader");
+  input.setAttribute("disabled", "disabled");
+  } else {
+  input.addEventListener("change", function(){
+    let file = this.files[0]
+    try{
+        if (!/image\/\w+/.test(file.type)) { //checking the file type
+          alert("Image only!");
+          return false;
+        }
+    
+        let reader = new FileReader(); //set a fileReader object
+        reader.readAsDataURL(file); //using readAsDataURL to read the image url
+        reader.onload = function(e) {
+          imghidden.value=this.result;//get the image base url so can use base64 to convert to image
+          console.log(this.result);
+        }
+
+    }catch(e){
+      imghidden.value="";//give blank if no image been choose
+    }
+
+    }, false);//if able to use, run the readFile function
+    
+  }
+
+
 // Helper function to check if the user is logged in
-const loggedIn = () => {
+function loggedIn() {
   const loginJSON = window.localStorage.getItem("login-data");
   return JSON.parse(loginJSON) || null;
 };
 
 // Logout function
-const logout = () => {
+function logout() {
   const loginData = loggedIn();
 
   const options = {
@@ -56,12 +194,12 @@ const logout = () => {
 };
 
 // Function to sort posts by most likes
-const filterByMostLikes = (posts) => {
+function filterByMostLikes(posts) {
   return posts.sort((a, b) => b.likes.length - a.likes.length);
 };
 
 // Function to fetch posts
-const fetchPosts = () => {
+function fetchPosts() {
   if (loggedIn() !== null) {
     const options = {
       method: "GET",
@@ -71,7 +209,7 @@ const fetchPosts = () => {
       },
     };
 
-    return fetch(apiBaseURL + "/api/posts", options)
+    return fetch(apiBaseURL + "/api/posts?limit=2000&offset=0", options)
       .then((response) => response.json())
       .catch((error) => {
         console.error("Error fetching posts:", error);
@@ -83,14 +221,14 @@ const fetchPosts = () => {
 };
 
 // Function to fetch recent posts
-const fetchRecentPosts = () => {
+function fetchRecentPosts() {
   return fetchPosts().then((data) => {
     return data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
   });
 }
 
 // Function to like a post
-const likePost = (post) => {
+function likePost(post) {
   if (loggedIn() !== null) {
     const options = {
       method: "POST",
@@ -111,7 +249,7 @@ const likePost = (post) => {
 };
 
 // Function to dislike a post
-const dislikePost = (post) => {
+function dislikePost(post) {
   if (loggedIn() !== null) {
     const likeToDelete = post.likes.find(
       (like) => like.username === loggedIn().username
@@ -135,7 +273,7 @@ const dislikePost = (post) => {
 };
 
 // Function to delete a post
-const deletePost = (post) => {
+function deletePost(post) {
   if (loggedIn() !== null) {
     const options = {
       method: "DELETE",
@@ -153,7 +291,7 @@ const deletePost = (post) => {
 };
 
 // Function to delete user's own post
-const deleteUserPost = (post) => {
+function deleteUserPost(post) {
   if (loggedIn() !== null && loggedIn().username === (post.username+"donut")) {
     const options = {
       method: "DELETE",
@@ -171,7 +309,7 @@ const deleteUserPost = (post) => {
 };
 
 // Function to create a post
-const createPost = (content) => {
+function createPost(content) {
   //store img data
 
   let imgBase64=imghidden.value
@@ -182,10 +320,6 @@ const createPost = (content) => {
   else {
     combineContent = content +imgBase64
   }
-  
-  
-
-
   
   imghidden.value="";
   input.value="";
@@ -206,7 +340,7 @@ const createPost = (content) => {
     fetch(apiBaseURL + "/api/posts", options)
       .then((response) => response.json())
       .then((data) => {
-        console.log("New post created:", data);
+        //console.log("New post created:", data);
         createPostInput.value = "";
         recentBtn.click()
         
@@ -220,229 +354,3 @@ const createPost = (content) => {
   }
   
 };
-
-// Check if the user is logged in
-const userLoggedIn = loggedIn();
-
-if (userLoggedIn) {
-  // Event listeners
-  recentBtn.addEventListener("click", () => {
-    fetchRecentPosts().then((data) => {
-     // console.log(data);
-      updateUI(data);
-    });
-  });
-
-  logoutBtn.addEventListener("click", logout);
-
-  // Retrieve recent posts and update UI
-  fetchRecentPosts().then((data) => {
-    //console.log(data);
-    updateUI(data);
-  });
-  
-
-  // Add event listener for form submission
-  postForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const postContent = createPostInput.value;
-    createPost(postContent);
-    // fetchRecentPosts().then((data) => {
-    //   //console.log(data);
-    //   updateUI(data);
-    // }
-    posts.forEach((post) => {
-      const { text, likes, username, createdAt, _id } = post;
-
-      // let text2=text;
-  
-      let contentHtml = ""
-
-      if(text.split("data:image").length==1){
-        //console.log(text)
-        contentHtml=`<p>${text.replaceAll("<","&lt;").replaceAll(">","&rt;").replace("data:image","")}</p>`
-      }else{
-       // console.log(text)
-        let textMain=text.split("data:image")[0];
-        let textImg=text.split("data:image")[1];
-        
-        //emoji use 
-        
-
-          // contentHtml=`<p>${textMain.replaceAll("<","&lt;").replaceAll(">","&rt;")}</p>`
-          //   +(textImg!=undefined?`<br><img src="data:image${textImg}">`:"")
-
-      }
-
-/* 
-        for(e of emojiGroup){
-          let emj=e
-        let eName=emj.emojiName
-        // console.log(emj)
-        //contentHtml=contentHtml.replaceAll("%"+eName+"%","okokok")
-        contentHtml=contentHtml.replaceAll("%"+eName+"%",`<img width=25 height=25 src="${emj.src}" class="emoji ${emj.id}"/>`)
-      } */
-      //一样个也。。点解上边可以replaceAll下面就no
-      const postDiv = document.createElement("div");
-      postDiv.className = "post";
-
-      const postContent = document.createElement("div");
-      postContent.className = "post-content";
-      postContent.innerHTML = contentHtml;
-
-      const postDetails = document.createElement("div");
-      postDetails.className = "post-details";
-      postDetails.innerHTML = `<span class="post-likes">${likes.length} likes</span> | <span class="post-author">${username}</span> | <span class="post-date">${createdAt}</span>`;
-
-      const likeBtn = document.createElement("button");
-      likeBtn.id = "like*"+_id;
-      likeBtn.addEventListener("click", () => likePost(post));
-      likeBtn.textContent = "Like";
-
-      const dislikeBtn = document.createElement("button");
-      dislikeBtn.id ="dislike*"+ _id;
-      dislikeBtn.addEventListener("click", () => dislikePost(post));
-      dislikeBtn.textContent = "Dislike";
-
-      const deleteBtn = document.createElement("button");
-      deleteBtn.id = "delete*"+ _id;
-      deleteBtn.addEventListener("click", () => deleteUserPost(post));
-      deleteBtn.textContent = "Delete";
-
-      postDiv.appendChild(postContent);
-      postDiv.appendChild(postDetails);
-      postDiv.appendChild(likeBtn);
-      postDiv.appendChild(dislikeBtn);
-      postDiv.appendChild(deleteBtn);
-
-      postsContainer.appendChild(postDiv);
-
-    });
-  }
-    
-    )
-    console.log (postContent);
-  };
-
-  // Function to update the UI with posts
-  const updateUI = (allPosts) => {
-
-    let posts=allPosts.filter(post=>{
-      if((new RegExp(/.*donut/)).test(post.username)){
-        post.username=post.username.substr(0,post.username.length-5);
-        //console.log(post.username)
-        return post
-      }
-    })
-
-    // if(isShowAll){
-    //   posts=allPosts
-    // }
-    postsContainer.innerHTML = "";
-
-    posts.forEach((post) => {
-      const { text, likes, username, createdAt, _id } = post;
-
-      // let text2=text;
-  
-      let contentHtml = ""
-
-      if(text.split("data:image").length==1){
-        //console.log(text)
-        contentHtml=`<p>${text.replaceAll("<","&lt;").replaceAll(">","&rt;").replace("data:image","")}</p>`
-      }else{
-       // console.log(text)
-        let textMain=text.split("data:image")[0];
-        let textImg=text.split("data:image")[1];
-        
-        //emoji use 
-        
-
-          // contentHtml=`<p>${textMain.replaceAll("<","&lt;").replaceAll(">","&rt;")}</p>`
-          //   +(textImg!=undefined?`<br><img src="data:image${textImg}">`:"")
-
-      }
-
-/* 
-        for(e of emojiGroup){
-          let emj=e
-        let eName=emj.emojiName
-        // console.log(emj)
-        //contentHtml=contentHtml.replaceAll("%"+eName+"%","okokok")
-        contentHtml=contentHtml.replaceAll("%"+eName+"%",`<img width=25 height=25 src="${emj.src}" class="emoji ${emj.id}"/>`)
-      } */
-      //一样个也。。点解上边可以replaceAll下面就no
-      const postDiv = document.createElement("div");
-      postDiv.className = "post";
-
-      const postContent = document.createElement("div");
-      postContent.className = "post-content";
-      postContent.innerHTML = contentHtml;
-
-      const postDetails = document.createElement("div");
-      postDetails.className = "post-details";
-      postDetails.innerHTML = `<span class="post-likes">${likes.length} likes</span> | <span class="post-author">${username}</span> | <span class="post-date">${createdAt}</span>`;
-
-      const likeBtn = document.createElement("button");
-      likeBtn.id = "like*"+_id;
-      likeBtn.addEventListener("click", () => likePost(post));
-      likeBtn.textContent = "Like";
-
-      const dislikeBtn = document.createElement("button");
-      dislikeBtn.id ="dislike*"+ _id;
-      dislikeBtn.addEventListener("click", () => dislikePost(post));
-      dislikeBtn.textContent = "Dislike";
-
-      const deleteBtn = document.createElement("button");
-      deleteBtn.id = "delete*"+ _id;
-      deleteBtn.addEventListener("click", () => deleteUserPost(post));
-      deleteBtn.textContent = "Delete";
-
-      postDiv.appendChild(postContent);
-      postDiv.appendChild(postDetails);
-      postDiv.appendChild(likeBtn);
-      postDiv.appendChild(dislikeBtn);
-      postDiv.appendChild(deleteBtn);
-
-      postsContainer.appendChild(postDiv);
-
-    });
-  };
-// else {
- // window.location.assign("./login.html");
-//}
-//} 
-
-  //for adding image to the post
-  if (typeof FileReader === "undefined") { //check able to use the fileReader
-    alert("This browser not support FileReader");
-    input.setAttribute("disabled", "disabled");
-  } else {
-    input.addEventListener("change", function(){
-      let file = this.files[0]
-// console.log(file)
-    try{
-        if (!/image\/\w+/.test(file.type)) { //checking the file type
-          alert("Image only!");
-          return false;
-        }
-    
-        let reader = new FileReader(); //set a fileReader object
-        reader.readAsDataURL(file); //using readAsDataURL to read the image url
-        reader.onload = function(e) {
-          imghidden.value=this.result;
-          //console.log(this.result);
-        }
-
-    }catch(e){
-      imghidden.value="";
-    }
-
-    }, false);//if able to use, run the readFile function
-    
-  }
-
-
-
-  //using base64 to upload image
-  //let base64Img = require('base64-img');
