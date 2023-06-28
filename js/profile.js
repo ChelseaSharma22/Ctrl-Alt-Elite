fffb76072041747fd63e1a0866eb578cf4d38653
Yuen-Zhang <yuen.en@hotmail.com>
2023-06-28 15:34:14 -0700

    profile update

--- js/profile.js
+++ js/profile.js
@@ -10,15 +10,55 @@ const recentBtn = document.getElementById("recent");
 const createPostInput = document.getElementById("createPost");
 const imghidden = document.getElementById("imghidden");
 
+//elements for user info
+const loginData = loggedIn();
+console.log(loginData);
+const currentUser = loginData.username.substr(0,loginData.username.length-5);
+const userFullName = document.getElementById("fullName");
+const userCreationDate = document.getElementById("createdAt");
+const signoutBtn = document.getElementById("signoutBtn");
+const bio = document.getElementById("bio");
+const fullNameInput = document.getElementById("fullNameInput");
+const bioInput = document.getElementById("bioInput");
+const saveEditsBtn = document.getElementById("editSaveBtn");
+const ProfileName = document.getElementById("ProfileName");
+
+//things todo when page load
+window.addEventListener("load", function () {
+  moreInfo();
+ 
+  saveEditsBtn.onclick = updateProfile;
+  document.getElementById("username").innerText = "@" + currentUser;
+  document.getElementById("username1").innerHTML = currentUser;
+
+});
+
+//Check if the user is logged in
+const userLoggedIn = loggedIn();
+
+ if (userLoggedIn) {
+   logoutBtn.addEventListener("click", logout);
+
+} else {
+  window.location.assign("./login.html");
+}
+
+//creat post
+postForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const postContent = createPostInput.value;
+  createPost(postContent);
+  
+});
 
 // Helper function to check if the user is logged in
-const loggedIn = () => {
+function loggedIn() {
   const loginJSON = window.localStorage.getItem("login-data");
   return JSON.parse(loginJSON) || null;
 };
 
 // Logout function
-const logout = () => {
+function logout() {
   const loginData = loggedIn();
 
   const options = {
@@ -36,37 +76,6 @@ const logout = () => {
       window.location.assign("login.html");
     });
 };
-
-// Function to sort posts by most likes
-const filterByMostLikes = (posts) => {
-  return posts.sort((a, b) => b.likes.length - a.likes.length);
-};
-
-
-//my code
-const loginData = loggedIn();
-console.log(loginData);
-const currentUser = loginData.username.substr(0,loginData.username.length-5);
-const userFullName = document.getElementById("fullName");
-const userCreationDate = document.getElementById("createdAt");
-const signoutBtn = document.getElementById("signoutBtn");
-const bio = document.getElementById("bio");
-const fullNameInput = document.getElementById("fullNameInput");
-const bioInput = document.getElementById("bioInput");
-const saveEditsBtn = document.getElementById("editSaveBtn");
-
-const ProfileName = document.getElementById("ProfileName");
-
-
-window.addEventListener("load", function () {
-    moreInfo();
-   
-    saveEditsBtn.onclick = updateProfile;
-    document.getElementById("name").innerHTML = "@" + currentUser;
-  
-
-
-});
 
 // Update Profile when Edit Profile Saved
 function updateProfile(event) {
@@ -115,27 +124,7 @@ function moreInfo() {
         });
 }
 
-
-
-//Check if the user is logged in
-const userLoggedIn = loggedIn();
-
- if (userLoggedIn) {
-
-
-   logoutBtn.addEventListener("click", logout);
-
-
-   
- 
-
-
-
-} else {
-  window.location.assign("./login.html");
-}
-  
-
+//create post function
 function createPost(content) {
   //store img data
 
@@ -167,11 +156,9 @@ function createPost(content) {
     fetch(apiBaseURL + "/api/posts", options)
       .then((response) => response.json())
       .then((data) => {
-        console.log("New post created:", data);
+        //console.log("New post created:", data);
         createPostInput.value = "";
-       
         window.location.href= "post.html";
-
       })
       .catch((error) => {
         console.error("Error creating post:", error);
@@ -181,13 +168,3 @@ function createPost(content) {
   }
   
 };
-
-postForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const postContent = createPostInput.value;
-  createPost(postContent);
-  
-});
- 
-
-
